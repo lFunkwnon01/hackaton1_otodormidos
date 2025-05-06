f9c8b13acd19e5bcdfb322a7de4cb11809f697fe
lFunkwnon01 <fabricio.ladera@utec.edu.pe>
2025-05-06 16:30:56 -0500

    creacion plantilla

---
+++ hacka/src/main/java/com/hacka/HackaApplication.java
@@ -0,0 +1,13 @@
+package com.hacka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackaApplication.class, args);
+    }
+
+}
